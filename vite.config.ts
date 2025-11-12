a8675af9706493089de20101ea958c7d8018e704
ERBODY <150860506+ERBODY@users.noreply.github.com>
2025-11-13 01:57:41 +0300

    fix: update vite config for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/fati/' : '/',
-  plugins: [
-    react(),
-
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: mode === "production" ? (process.env.BASE_URL ?? "/fati/") : "/",
+  plugins: [react()],
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 }));
